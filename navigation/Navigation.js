9729298d0b4ba280743108432081860abeaf60c9
hobiana <hobianarazakanaivo@gmail.com>
2020-12-11 11:48:00 +0400

    update navigation

--- navigation/Navigation.js
+++ navigation/Navigation.js
@@ -7,6 +7,9 @@ import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/home/Home';
 import SettingsScreen from '../screens/setting/Setting';
 import ProfileScreen from '../screens/profile/Profile'
+import EpisodesScreen from '../screens/episodes/Episodes'
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Feather from 'react-native-vector-icons/Feather';
 
 
 const Tab = createBottomTabNavigator();
@@ -24,9 +27,42 @@ function Root(props) {
 export default function Navigation() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Character" component={Root} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+      <Tab.Navigator tabBarOptions={{
+        labelStyle: { textTransform: "none", },
+        style: {
+          color: '#292D3E'
+        }
+      }}>
+        <Tab.Screen
+          name="Character"
+          component={Root}
+          options={{
+            tabBarBadgeStyle: '#292D3E',
+            tabBarIcon: () => (
+              <Icon name="users" color={'#292D3E'} size={25} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Episodes"
+          component={EpisodesScreen}
+          options={{
+            tabBarBadgeStyle: '#292D3E',
+            tabBarIcon: () => (
+              <Icon name="tv" color={'#292D3E'} size={25} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Settings"
+          component={SettingsScreen}
+          options={{
+            tabBarBadgeStyle: '#292D3E',
+            tabBarIcon: () => (
+              <Feather name="settings" color={'#292D3E'} size={25} />
+            ),
+          }}
+        />
       </Tab.Navigator>
 
     </NavigationContainer>
